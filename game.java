96c1cd756e4ca9a0bb17d04dab98e92a690efc1d
rahulYUV <rs6205903996@gmail.com>
2024-10-18 00:48:49 +0530

    updated game

--- game.java
+++ game.java
@@ -1,7 +1,7 @@
 class game {
     public static void main(String args[]){
         // String $ame = "rahul ";
-        // System.out.println($ame);
+        
         // String Name = "akash";
         // int roll =45;
         // System.err.println(Name);
@@ -18,13 +18,22 @@ class game {
 
             // char ate = 'a';
             // System.out.println(&ate);                
-                int m =10;
-                int m1,m2,m3,m4,m5;
-                m1=m2=m3=m4=m5=m;
-                System.out.println(m1);
-                System.out.println(m2);
-                // System.out.println(m1+=m2);
-                System.out.println(m2%m1+m1);
+                // int m =10;
+                // int m1,m2,m3,m4,m5;
+                // m1=m2=m3=m4=m5=m;
+                // System.out.println(m1);
+                // System.out.println(m2);
+                // // System.out.println(m1+=m2);
+                // System.out.println(m2%m1+m1);
+                int a,b;
+                a=10;
+                b=15;
+                
+                System.out.println((a + b) + " add");//25
+                System.out.println(a*b + "mul"); // 150
+                System.out.println(a%b+"division");//
+                System.out.println(a-b+"minus"); //-5
+
 
         }
 }
