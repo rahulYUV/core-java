d9614d9229a30e8c9f00a72b226a0aad314597ff
rahulYUV <rs6205903996@gmail.com>
2024-10-18 00:39:15 +0530

    game updated

--- game.java
+++ game.java
@@ -1,12 +1,30 @@
 class game {
     public static void main(String args[]){
-        String $ame = "rahul ";
-        System.out.println($ame);
-        String Name = "akash";
-        int roll =45;
-        System.err.println(Name);
-        // java is case sensitive
+        // String $ame = "rahul ";
+        // System.out.println($ame);
+        // String Name = "akash";
+        // int roll =45;
+        // System.err.println(Name);
+        // // java is case sensitive
 
-        
-    }
+        //data types 
+        //primitive and non primittive 
+        // short a = 1234;
+        // System.out.println(a);
+            // float 
+            //  double data =45.5;
+            // System.out.println(data);
+                // boolean temp  = false;
+
+            // char ate = 'a';
+            // System.out.println(&ate);                
+                int m =10;
+                int m1,m2,m3,m4,m5;
+                m1=m2=m3=m4=m5=m;
+                System.out.println(m1);
+                System.out.println(m2);
+                // System.out.println(m1+=m2);
+                System.out.println(m2%m1+m1);
+
+        }
 }
