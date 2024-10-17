a1b5724fa4d076e8907754e864e6d60b3440ca67
rahulYUV <rs6205903996@gmail.com>
2024-10-17 23:01:21 +0530

    initial commit

---
+++ game.java
@@ -0,0 +1,12 @@
+class game {
+    public static void main(String args[]){
+        String $ame = "rahul ";
+        System.out.println($ame);
+        String Name = "akash";
+        int roll =45;
+        System.err.println(Name);
+        // java is case sensitive
+
+        
+    }
+}
