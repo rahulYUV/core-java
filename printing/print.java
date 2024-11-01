c4de001d091357a89f66515803cf250bad25105d
rahulYUV <rs6205903996@gmail.com>
2024-11-01 19:49:20 +0530

    FirstCommit

---
+++ printing/print.java
@@ -0,0 +1,27 @@
+
+
+public class print {
+    public static void main(String args[]){
+        int a =10;
+        System.out.print(a);
+        System.out.print(a);
+        System.out.println(a);
+        System.out.print(a);
+        // print(we cannot use comma)
+        System.out.println(a);
+        System.out.println("Sum:"+10+20);
+        System.out.println(10+20+" is the addition ");// left to right 
+        int y= 10;
+        int x =20;
+        System.out.println("sum of the " + x+" and "+y+ "is  :"+ (x+y));
+
+
+        // printf
+                System.out.printf("%d",4);
+                System.out.printf("\n");
+                System.out.printf("hellow");
+       
+    }
+    
+    
+}
