e7c48bf13ec291fbf1a6010936a72c311b03c220
rahulYUV <rs6205903996@gmail.com>
2024-11-04 17:55:31 +0530

    stringCommit

--- printing/string.java
+++ printing/string.java
@@ -1,23 +1,38 @@
+import java.util.Scanner;
 
-public class string {
-    public static void main(String args[]){
-        // String str1 = "java prog";// java program stored in pool
-        // System.out.println(str1);// refering to java prog.
-        // String str2=new String("java") ;
-        // System.out.println(str2);
+public class string{
+    public static void main(String args[]) {
+        String input;
+        String sentence = "my name is tony stark";
+
+        // Initialize the scanner
+        Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Please enter the sentence 'my name is tony stark':");
+
+    //     // Use nextLine to capture the whole line from user input
+    //     input = sc.nextLine();
+
+    //     // Compare the input with the predefined sentence
+    //     if (input.equals(sentence)) {
+    //         System.out.println("Input matches: " + input);
+    //     } else {
+    //         System.out.println("Input does not match. You entered: " + input);
+    //     }
+
+    //     // Close the scanner
+    //     sc.close();
+    //
+          String first= "tony";
+          String second = "stark";
+          String third = first+second;
+         
+     
+          //System.out.println(third.charAt(5));
+          String avg = "tony ";
+          String abc ="tony";
+          System.out.println(avg.compareTo(abc));
 
 
-        String name = " thisIsMehtod";
-        int a = 10;
-        System.out.println(name.length());
-        System.out.println(name.toLowerCase());
-        String Name = name.toUpperCase();
-        System.out.println(Name);
-        
-        name = name.toLowerCase();
-        System.out.println(name);
-        String trimmer = " java program";
-        System.out.println(trimmer.trim());//
-
-    } 
+    }
 }
