684791266ab64ada8d74abda5a69669a2cb2228a
rahulYUV <rs6205903996@gmail.com>
2024-11-06 23:42:18 +0530

    string pratice

--- printing/string.java
+++ printing/string.java
@@ -2,37 +2,97 @@ import java.util.Scanner;
 
 public class string{
     public static void main(String args[]) {
-        String input;
-        String sentence = "my name is tony stark";
+    //     String input;
+    //     String sentence = "my name is tony stark";
 
-        // Initialize the scanner
-        Scanner sc = new Scanner(System.in);
+    //     // Initialize the scanner
+    //     Scanner sc = new Scanner(System.in);
 
-    //     System.out.println("Please enter the sentence 'my name is tony stark':");
+    // //     System.out.println("Please enter the sentence 'my name is tony stark':");
 
-    //     // Use nextLine to capture the whole line from user input
-    //     input = sc.nextLine();
+    // //     // Use nextLine to capture the whole line from user input
+    // //     input = sc.nextLine();
 
-    //     // Compare the input with the predefined sentence
-    //     if (input.equals(sentence)) {
-    //         System.out.println("Input matches: " + input);
-    //     } else {
-    //         System.out.println("Input does not match. You entered: " + input);
-    //     }
+    // //     // Compare the input with the predefined sentence
+    // //     if (input.equals(sentence)) {
+    // //         System.out.println("Input matches: " + input);
+    // //     } else {
+    // //         System.out.println("Input does not match. You entered: " + input);
+    // //     }
 
-    //     // Close the scanner
-    //     sc.close();
-    //
-          String first= "tony";
-          String second = "stark";
-          String third = first+second;
+    // //     // Close the scanner
+    // //     sc.close();
+    // //
+    //       String first= "tony";
+    //       String second = "stark";
+    //       String third = first+second;
          
      
-          //System.out.println(third.charAt(5));
-          String avg = "tony ";
-          String abc ="tony";
-          System.out.println(avg.compareTo(abc));
+    //       //System.out.println(third.charAt(5));
+    //       String avg = "tony ";
+    //       String abc ="tony";
+    //       System.out.println(avg.compareTo(abc));
 
 
+
+            // String sentence =" my name is tony stark";
+            // System.out.println(sentence.substring(1,10));
+
+
+          //   Scanner sc = new Scanner(System.in);
+          // String str = sc.next();
+          // String result = " ";
+          // for(int i =0;i<str.length();i++){
+          //   if(str.charAt(i)=='e'){
+          //     // System.out.println("E occured what you want to replace ");
+          //    result += 'i';
+          //   }
+          //    else
+          //    {
+          //           result+= str.charAt(i);
+          //    }
+
+
+            
+          // }
+
+
+          // System.out.println(result);
+
+
+          // String  str1= "java";
+          // String str2 = "java";
+          
+          // String str3= new String(" core java ");
+          // String str4 = new String("core java");
+            // if(str3 == str4){
+            //   System.out.println("equal");
+            // }else{
+            //   System.out.println(" not equi");
+        // char a[]={"h","e","l","o"};
+        //   String str 5 = new String(a);
+        // System.out.println(" ");
+        // byte b[]={65, 67, 50, 88};
+        // String str5 = new String(b);
+        // System.out.println(str5);
+
+
+
+
+      //   String str1 = "java";
+      //   String str2 = "javascript";
+
+      //   String str3 = "javaaa";
+      //   String str4= "javaa";
+
+
+      //  int n =  str3.compareTo(str4);
+      //  System.out.println(n);
+
+
+      String str1 = "pyramid";
+      String str2 = "pyramid";
+      System.out.println(str1.equals(str2));// it als check space 
+      System.out.println(str1.compareTo(str2));
     }
 }
