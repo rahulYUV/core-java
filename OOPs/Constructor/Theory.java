3e3760021dc0cd3c5d6d50724dd96a839723ec08
rahulYUV <rs6205903996@gmail.com>
2025-03-15 16:21:59 +0530

    construcotr Started

---
+++ OOPs/Constructor/Theory.java
@@ -0,0 +1,64 @@
+/*
+    Java Constructors 
+    What is a Constructor?
+    ----------------------
+    A constructor in Java is a special method used to initialize objects.
+    It is called when an object of a class is created. 
+    Constructors have the same name as the class and do not have a return type.
+    
+    Characteristics of Constructors:
+    - They have the same name as the class.
+    - They do not have a return type (not even 'void').
+    - They are automatically invoked when an object is created.
+    - If no constructor is defined, the Java compiler provides a default constructor.
+*/
+
+/* 
+    Types of Constructors in Java
+    -----------------------------
+    1. Default Constructor
+    2. Parameterized Constructor
+    3. Constructor Overloading
+    4. Copy Constructor
+    5. Constructor Chaining (using 'this()')
+*/
+
+
+public class Theory{
+    public static void main(String []args){
+
+    }
+}
+
+/* 
+    Interview Questions and Answers on Constructors
+    -----------------------------------------------
+    1. What is a Constructor in Java?
+       - A constructor is a special method used to initialize an object automatically
+         when an instance of a class is created.
+
+    2. Can a Constructor be 'final', 'static', or 'synchronized'?
+       - No, a constructor cannot be 'final', 'static', or 'synchronized'.
+
+    3. Can a Constructor be Overloaded?
+       - Yes, constructor overloading is possible in Java by defining multiple constructors
+         with different parameter lists.
+
+    4. What is Constructor Chaining?
+       - Constructor chaining is calling one constructor from another constructor using 'this()'.
+
+    5. Difference Between Constructor and Method?
+       | Feature        | Constructor | Method |
+       |---------------|------------|--------|
+       | Purpose       | Initializes an object | Performs a task |
+       | Return Type   | No return type | Can have a return type |
+       | Name          | Same as class | Any valid name |
+       | Call          | Called automatically on object creation | Called explicitly |
+*/
+
+/* 
+    Additional Learning Resources:
+    ------------------------------
+    
+    3. Java Cheat Sheet (PDF): https://cheatography.com
+*/
